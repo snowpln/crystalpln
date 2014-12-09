5b524186bc5acd789bff9e1a1434d4796af6cc5c
snowpln <214349063@qq.com>
2014-12-09 12:40:49 +0800

    update throw

--- grails-app/services/interview/BlogFacadeService.groovy
+++ grails-app/services/interview/BlogFacadeService.groovy
@@ -7,27 +7,28 @@ import grails.transaction.Transactional
 @Transactional
 class BlogFacadeService implements BlogFacade {
 
-    @Override
-    void publish(BlogEntryDTO entry) { //≤∂ªÒ“Ï≥£
-        try {
-            new BlogEntry(title: entry.title, content: entry.content, dateCreated: entry.dateCreated).save()
-        } catch (Exception e) {
-            println "±£¥Ê ß∞‹"
-        }
-    }
+	@Override
+	void publish(BlogEntryDTO entry) { //≤∂ªÒ“Ï≥£
+		try {
+			new BlogEntry(title: entry.title, content: entry.content, dateCreated: entry.dateCreated).save()
+		} catch (Exception e) {
+			println "±£¥Ê ß∞‹"
+			throw new RuntimeException("±£¥Ê ß∞‹")
+        	}
+	}
 
-    @Override
-    List<BlogEntryDTO> getRecentEntries(int n) {
-        def blogEntryDTOList = new ArrayList<BlogEntryDTO>()
-        def blogList = BlogEntry.listOrderBydateCreated(max: n, offset: 0, order: "desc")
-        blogList.each { blog ->
-            blogEntryDTOList.add(toDTO(blog))
-        }
-        return blogEntryDTOList
-    }
+	@Override
+	List<BlogEntryDTO> getRecentEntries(int n) {
+		def blogEntryDTOList = new ArrayList<BlogEntryDTO>()
+		def blogList = BlogEntry.listOrderBydateCreated(max: n, offset: 0, order: "desc")
+		blogList.each { blog ->
+		blogEntryDTOList.add(toDTO(blog))
+		}
+		return blogEntryDTOList
+	}
 
-    BlogEntryDTO toDTO(BlogEntry entry) {
-        def blogEntryDTO = new BlogEntryDTO(title: entry.title, content: entry.content, dateCreated: entry.dateCreated)
-        return blogEntryDTO
-    }
+	BlogEntryDTO toDTO(BlogEntry entry) {
+		def blogEntryDTO = new BlogEntryDTO(title: entry.title, content: entry.content, dateCreated: entry.dateCreated)
+		return blogEntryDTO
+	}
 }
